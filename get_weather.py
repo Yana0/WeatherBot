0a1acf3e7110e1049ec12eff47fc3cc58b73b79b
Yana0 <37939863+Yana0@users.noreply.github.com>
2021-04-25 20:58:18 +0300

    Fix

--- get_weather.py
+++ get_weather.py
@@ -2,15 +2,16 @@ import requests
 from bs4 import BeautifulSoup as BS
 import datetime
 
+url = 'https://sinoptik.ua/погода-долгопрудный/'
+
 
 def degrees_to_int(string):
-    tmp = string.split(' ')
-    tmp = tmp[1:-1]
-    tmp = [int(i[:-1]) for i in tmp]
-    return tmp
+    """convert Celsius degrees to integer"""
+    return [int(i[:-1]) for i in string.split(' ')[1:-1]]
 
 
 def int_to_degrees(temp):
+    """convert integer to Celsius degrees"""
     ans = '+' + str(temp) + '°'
     if temp < 0:
         ans[0] = '-'
@@ -20,7 +21,8 @@ def int_to_degrees(temp):
 
 
 def get_weather(days):
-    url = 'https://sinoptik.ua/погода-долгопрудный/'
+    """Return weather for next days: min temperature, max temperature, text description,
+    folk weather forecast(for current day)"""
     current_day = datetime.datetime.today()
     msg_start = "Погода на: "
     msg_res = ""
@@ -30,14 +32,13 @@ def get_weather(days):
         html = BS(request.content, 'html.parser')
         t_min = t_max = text = mem_text = ""
         for el in html.select('#content'):
-            #t_min = [str(i).isdigit() for i in el.select('.weatherDetails .temperature')[0].text]
             temperature = degrees_to_int(el.select('.weatherDetails .temperature')[0].text)
             t_min = int_to_degrees(min(temperature))
             t_max = int_to_degrees(max(temperature))
             text = el.select('.wDescription .description')[0].text
             mem_text = el.select('.oDescription .description')[0].text
 
-        msg_res += "мин: " + t_min + ', ' + "макс: " + t_max + '\n' + text + '\n\n'
+        msg_res += '+'.join(["мин: ", t_min, ', ', "макс: ", t_max, '\n', text, '\n\n'])
         if days == 1:
             msg_res += mem_text + '\n\n'
         current_day += datetime.timedelta(days=1)
