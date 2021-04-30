2c6f1b099f58d4f46b54fc0de62ba847886e5d3a
Yana0 <37939863+Yana0@users.noreply.github.com>
2021-04-30 14:53:40 +0300

    add tests

---
+++ Tests.py
@@ -0,0 +1,28 @@
+import unittest
+import get_weather
+
+
+class TestInt2Degrees(unittest.TestCase):
+    """Test convert int to degrees"""
+    def test_positive(self):
+        self.assertEqual(get_weather.int_to_degrees(100), '+100°')
+
+    def test_negative(self):
+        self.assertEqual(get_weather.int_to_degrees(-100), '-100°')
+
+    def test_zero(self):
+        self.assertEqual(get_weather.int_to_degrees(0), '0°')
+
+
+class TestDegrees2Int(unittest.TestCase):
+    """Test convert degrees to int"""
+    def test_positive(self):
+        self.assertEqual(get_weather.degrees_to_int('+10°'), 10)
+
+    def test_negative(self):
+        self.assertEqual(get_weather.degrees_to_int('-10°'), -10)
+
+    def test_zero(self):
+        self.assertEqual(get_weather.degrees_to_int('0°'), 0)
+
+
